db7b3dd9dd5a4c15a19f15f80cf3110185e2e20a
PintaPontus <matteo.blascoviz@gmail.com>
2024-04-10 19:56:32 +0200

    start both applications simultaneously

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,51 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::Arc;
+use std::thread;
+
+use crate::controller::hello_world;
+use crate::tauri_layer::log_application;
+use axum::routing::get;
+use axum::Router;
+use tauri::App;
+use tokio::net::TcpListener;
+use tokio::runtime::Runtime;
+
 mod controller;
 mod tauri_layer;
-mod listener;
-
-use tauri_layer::log_application;
-use listener::start_axum;
 
 #[tokio::main]
 async fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            log_application,
-            start_axum
-        ]).run(tauri::generate_context!())
+    let tauri_app = tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![log_application])
+        .build(tauri::generate_context!())
         .expect("error while building tauri application");
+
+    let axum_app = Router::new()
+        .route("/", get(hello_world))
+        // .route("/message", post(post_message))
+        .with_state(Arc::new(tauri_app.handle()));
+
+    // Spawn one separate threads for axum server
+    let axum_handle = thread::spawn(|| {
+        Runtime::new().unwrap().block_on(start_axum(axum_app));
+    });
+    start_tauri(tauri_app).await;
+
+    axum_handle.join().unwrap();
+}
+
+async fn start_tauri(tauri_app: App) {
+    println!("Starting Tauri");
+    tauri_app.run(|_app_handle, event| match event {
+        _ => {}
+    });
+}
+
+async fn start_axum(axum_app: Router) {
+    println!("Starting Axum");
+    // run our app with hyper, listening globally on port 8000
+    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    axum::serve(listener, axum_app).await.unwrap();
 }
